d8f2e940123e7f5204290e558da84d3eb76af660
sopi <sopisofi22@gmail.com>
2025-10-25 19:49:18 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class mahasiswa{
+    static String nama;
+    static String nim;
+
+    public mahasiswa(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+
+    public static boolean login(String inputnama, String inputnim){
+        return inputnama.equals(nama) && inputnim.equals(nim);
+    }
+
+    public void display_info(){
+        System.out.println("Login berhasil!");
+        System.out.println("Nama " + nama);
+        System.out.println("Nim " + nim);
+    }
+}
